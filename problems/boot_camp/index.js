fc9cd0a12e678ced8d407eabe32bc3624b011afa
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-10-09 02:59:23 +0200

    first problem just uses @substack's browserify-adventure for reference

---
+++ problems/boot_camp/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var fs = require('fs');
+var verify = require('adventure-verify');
+var unpack = require('browser-unpack');
+var concat = require('concat-stream');
+var api = module.exports;
+
+api.problem = fs.createReadStream(__dirname + '/problem.txt');
+api.solution = fs.createReadStream(__dirname + '/solution.txt');
+
+api.verify = verify({ modeReset: true }, function (args, t) {
+  t.plan(4);
+  process.stdin.pipe(concat(function (body) {
+    var rows;
+    try {
+      rows = unpack(body);
+    } catch (err) {
+      return t.fail('The input had a syntax error!');
+    }
+    if (!rows) {
+      return t.fail('The input is not a browserify bundle!');
+    }
+
+    t.equal(rows.length, 1, 'a single file in this bundle');
+    t.equal(rows[0].entry, true, 'single file should be an entry file');
+    t.deepEqual(rows[0].deps, {}, 'shouldn\'t have any deps');
+
+    Function(['console'], body.toString())({
+      log: function (msg) { t.equal(msg, 'PONY FOO') },
+        error: console.error
+      });
+  }));
+});
+
+api.run = function (args) {
+  process.stdin.pipe(concat(function (body) {
+    Function(body.toString())();
+  }));
+};
---
+++ test/solutions.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var test = require('tape');
+var spawn = require('child_process').spawn;
+var browserify = require('browserify');
+var ignored = /^!/;
+var names = require('../menu.json').filter(unignored);
+var args = process.argv.slice(2);
+if (args.length) {
+  names = args;
+}
+
+var solutions = names.map(expand);
+var customBuild = {
+};
+
+var customRun = {
+};
+
+
+test(plan);
+
+function expand (name) {
+  return path.join(__dirname, '../solutions', name.toLowerCase().replace(/\W+/g, '_'));
+}
+
+function unignored (name) {
+  return !ignored.test(name);
+}
+
+function plan (t) {
+  t.plan(names.length * 2);
+
+  (function next () {
+    if (names.length === 0) {
+      return;
+    }
+    var name = names.shift();
+    var solution = solutions.shift();
+    var main = path.join(solution, 'main.js');
+
+    cmd(['select', name]).on('exit', assertion);
+
+    function assertion (code) {
+      t.equal(code, 0, 'select ' + name);
+
+      var ps = cmd('verify');
+      var b;
+
+      ps.stderr.pipe(process.stderr);
+      ps.stdout.pipe(process.stderr);
+      ps.on('exit', exited);
+
+      if (customBuild[name]) {
+        b = customBuild[name](solution);
+      } else {
+        b = browserify(main).bundle();
+      }
+      b.pipe(ps.stdin);
+
+      if (customRun[name]) {
+        customRun[name](t);
+      }
+    }
+
+    function exited (code) {
+      t.equal(code, 0, 'verify ' + name);
+      next();
+    }
+  })();
+}
+
+function cmd (args) {
+  var script = path.join(__dirname, '../bin/pony');
+  return spawn(process.execPath, [script].concat(args));
+}
